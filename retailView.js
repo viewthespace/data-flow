294b7af1247fb78f73ff14d573b36f355f1f9016
Alex Weisberger <alex.weisberger@viewthespace.com>
2017-08-18 08:48:30 -0400

    Add more data to SpaceDetail

--- retailView.js
+++ retailView.js
@@ -16,13 +16,33 @@ class RetailViewCtrl {
       {
         tenant: "Starbucks",
         rent: "$1,000",
-        expirationDate: "1/1/2020"
+        expirationDate: "1/1/2020",
+        occupancyCost: "10%",
       },
       {
         tenant: "Target",
         rent: "$2,000",
-        expirationDate: "2/2/2025"
-      }
+        expirationDate: "2/2/2025",
+        occupancyCost: "30%",
+      },
+      {
+        tenant: "Best Buy",
+        rent: "$1,800",
+        expirationDate: "3/14/2027",
+        occupancyCost: "5%",
+      },
+      {
+        tenant: "Marshalls",
+        rent: "$1,100",
+        expirationDate: "2/10/2018",
+        occupancyCost: "15%",
+      },
+      {
+        tenant: "Chipotle",
+        rent: "$1,300",
+        expirationDate: "9/16/2029",
+        occupancyCost: "10%",
+      },
     ];
   }
 }
