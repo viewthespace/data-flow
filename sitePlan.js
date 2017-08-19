a2d7a6f984c8814d2f42bfdcbd1b6fc85f580a19
Alex Weisberger <alex.weisberger@viewthespace.com>
2017-08-19 18:44:28 -0400

    Highlight selected space in site plan

--- sitePlan.js
+++ sitePlan.js
@@ -3,8 +3,17 @@ class SitePlanCtrl {
     this.onSpaceSelected({ space: this.spaces[index] });
   }
 
-  spaceColor(index) {
+  spaceStyles(index) {
+    let styles = [this.spaceColor(index)];
     let space = this.spaces[index];
+    if (this.selectedSpace && space.tenant === this.selectedSpace.tenant) {
+      styles.push("active-space");
+    }
+
+    return styles;
+  }
+
+  spaceColor(index) {
     return [
       "btn-primary", "btn-danger", "btn-success",
       "btn-warning", "btn-secondary", "btn-info"
